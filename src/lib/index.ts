a729628cce4ac146d449a13b7e6be694ebc74a94
Emrah Esati <E23441@eon.com>
2024-07-20 19:37:41 +0200

    feat: removed comments

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,1 @@
-// Exportieren der Svelte-Komponenten
 export { default as RectangleSelector } from './RectangleSelector.svelte';
-// Fügen Sie weitere Komponenten hinzu, die Sie exportieren möchten
