8cd8a5e1fb30fc2a73fc21ccb5baeabeb2c2eb52
emrahes <emrah.esati@amperias.com>
2024-07-20 18:40:00 +0200

    feat: code atm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
     import RectangleSelector from "$lib/RectangleSelector.svelte";
+    import type { Rectangle } from "../types/Rectangles.js";
+
+    let rectangles: Rectangle;
 </script>
 
-<RectangleSelector />
+<div style="margin-top: 300px;">
+    <RectangleSelector {rectangles}>
+        <div style="height: 500px; width: 500px; background: green;">
+        </div>
+    </RectangleSelector>
+
+</div>
