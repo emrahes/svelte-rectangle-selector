1a5a35e03b15490aa76dd20891f90a07308c9daf
emrahes <emrah.esati@amperias.com>
2024-07-22 17:27:11 +0200

    feat: adjust release.config.cjs for release

--- release.config.cjs
+++ release.config.cjs
@@ -1,5 +1,5 @@
 const config = {
-    branches: ['main'],
+    branches: ['feature/add-semantic-release', 'main'],
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
